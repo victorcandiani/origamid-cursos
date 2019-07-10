22dba018b51d626bd3676cc74ca9b54ad56040a0
Victor Candiani <48362384+victorcandiani@users.noreply.github.com>
2019-07-10 16:43:16 -0300

    Curso de Wordpress como CMS
    Instalação do Local By FlyWheel;
    Instalação do Wordpress;
    Migrando do HTML para o PHP
    Instalação do Plugin Advanced Custom Fields
    Criação dos Campos personalizados para o final user

---
+++ 06-Wordpress-como-CMS/Rest/footer.php
@@ -0,0 +1,14 @@
+		<footer>
+			<nav class="nav-footer">
+				<ul>
+					<li class="current_page_item"><a href="/">Menu</a></li>
+					<li><a href="/testewp.local/sobre">Sobre</a></li>
+					<li><a href="/testewp.local/contato">Contato</a></li>
+				</ul>
+			</nav>
+
+			<p>Rest © 2015. Alguns direitos reservados.</p>
+		</footer>
+	<?php wp_footer(); ?>
+	</body>
+</html>
